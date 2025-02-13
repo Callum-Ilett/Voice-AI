15b8aa916ba732a26eff43061e1239b38a4cde19
Callum Ilett(Front-End Developer) <cilett99@gmail.com>
2025-02-13 22:16:46 +0000

    🔧 Update ESLint and TypeScript absolute path imports

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,9 @@
 // https://docs.expo.dev/guides/using-eslint/
 module.exports = {
   extends: "expo",
+  parserOptions: {
+    project: "./tsconfig.json",
+  },
   ignorePatterns: ["/dist/*"],
   overrides: [
     {
